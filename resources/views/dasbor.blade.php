ffb0449a657c4987cbbf3b94998d95c4523dd240
unknown <andre wira>
2019-10-21 23:22:52 +0700

    perbaiki dasbor

--- resources/views/dasbor.blade.php
+++ resources/views/dasbor.blade.php
@@ -6,5 +6,32 @@
 @endsection
 @section('menu_dasbor', 'active')
 @section('data_content')
-    
+    <div class="col-md-12 border" >
+        <br>
+        <br>
+        <div class="col-md-12">
+            <div class="alert alert-primary col-md-12" role="alert">
+                div alert
+            </div>
+            <h4 class="lead">daftar progja</h4>
+        </div>
+        <div class=" row col-md-12">
+
+            <div class="col-md-6">
+                <small class="text-muted" >Contoh tanggal mulai</small> 
+            </div>
+            <div class="col-md-6 text-right">
+                <small class="text-muted">Contoh tanggal selesai</small>
+                
+            </div>
+
+        </div>
+        <div class="col-md-12">
+            <label for="customRange2" >Progress</label>
+            <input type="range" class="custom-range" disabled value="25" min="0" max="100" id="customRange2">
+        </div>
+        <div class="col-md-12">
+            Tahap laporan
+        </div>
+    </div>
 @endsection
